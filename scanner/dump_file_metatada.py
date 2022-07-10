fb1569676c4dba042b41e7d1ef0e57c46c1b2a78
Walker Hale IV <walker.hale.iv@gmail.com>
2022-07-10 16:58:58 -0500

    Enhanced output

--- scanner/dump_file_metatada.py
+++ scanner/dump_file_metatada.py
@@ -1,11 +1,14 @@
 from multiprocessing import Process, Queue
 from pathlib import Path
 from queue import Empty
+from sys import stdout
 
 from .local_file_metadata import LocalFileMetadata, scan
 
 
 def run(start_dir_path: Path):
+    print(*LocalFileMetadata.field_names(), sep="\t")
+    stdout.flush()
     for r in scan(start_dir_path):
         r.set_md5_from_contents()
-        print(r.mode, r.md5, f"{r.num_bytes:7d}", r.path, sep="\t")
+        print(*r.astuple, sep="\t")
