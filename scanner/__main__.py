e1e1ec8ab5f2de8df072ffc587b6825e5108366c
Walker Hale IV <walker.hale.iv@gmail.com>
2022-07-10 16:47:28 -0500

    Remove call to .resolve

--- scanner/__main__.py
+++ scanner/__main__.py
@@ -6,7 +6,7 @@ from .dump_file_metatada import run
 
 def main():
     _, start_dir = argv
-    start_dir_path = Path(start_dir).resolve()
+    start_dir_path = Path(start_dir)
     run(start_dir_path)
 
 
